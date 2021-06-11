73fe6fb2e8de20dfd3e3697cf6a6c10c112a6bbf
Una Ada <trewbot@gmail.com>
2021-06-10 23:44:25 -0700

    feat(router): adds DELETE /skills/:id route

--- routes/skills.js
+++ routes/skills.js
@@ -18,6 +18,7 @@ router.get('/', skillsCtrl.index);
 router.post('/', skillsCtrl.create);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
+router.delete('/:id', skillsCtrl.deleteOne);
 
 /*----- Exports --------------------------------------------------------------*/
 module.exports = router;
