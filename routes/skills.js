c855c4be68e0975a77261f9f5aab88da380cd2fa
Una Ada <trewbot@gmail.com>
2021-06-10 23:07:33 -0700

    feat(router): adds POST /skills/ route

--- routes/skills.js
+++ routes/skills.js
@@ -15,6 +15,7 @@ const express = require('express'),
 
 /*----- Routes ---------------------------------------------------------------*/
 router.get('/', skillsCtrl.index);
+router.post('/', skillsCtrl.create);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
 
