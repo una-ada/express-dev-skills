891de2430fffb82b2a129baa148a543410401861
Una Ada <trewbot@gmail.com>
2021-06-10 23:17:29 -0700

    feat(model): adds create skill method

--- models/skill.js
+++ models/skill.js
@@ -13,13 +13,13 @@
  * @typedef {Object} Skill
  * @property {number} id A unique integer ID.
  * @property {string} skill A name for the skill.
- * @property {number} confidence How adepts I am at the skill.
+ * @property {number} confidence How adept you are at the skill.
  */
 /** @const {Skill[]} skills Skills list. */
 const skills = [
-  { id: 1, skill: 'JavaScript', confidence: 5 },
-  { id: 2, skill: 'HTML', confidence: 5 },
-  { id: 3, skill: 'CSS', confidence: 5 },
+  { id: 148755, skill: 'JavaScript', confidence: 5 },
+  { id: 150269, skill: 'HTML', confidence: 5 },
+  { id: 151663, skill: 'CSS', confidence: 5 },
 ];
 
 /*----- Methods --------------------------------------------------------------*/
@@ -37,10 +37,22 @@ const getAll = function () {
    */
   getOne = function (id) {
     return skills.find(skill => skill.id === +id);
+  },
+  /**
+   * Create a new Skill.
+   * @param {Object} skill The skill data object.
+   * @param {string} skill.skill The name of the skill.
+   * @param {number} skill.confidence How adept you are at the skill.
+   */
+  create = function (skill) {
+    // Add ID to skill
+    skill.id = +new Date() % 1e6;
+    skills.push(skill);
   };
 
 /*----- Exports --------------------------------------------------------------*/
 module.exports = {
   getAll,
   getOne,
+  create,
 };
